5faf8d1e922cef602bb7f5ffa7fbe72fe1111591
BettK <kelvin.bett@student.moringaschool.com>
2021-08-01 12:52:25 +0300

    added instance variables

--- pass.py
+++ pass.py
@@ -1,3 +1,11 @@
 class User:
-
+    '''
+    this is a class for adding user 
+    '''
+    def__init__(self,email,password,confirmPassword)
+    self.email=email
+    self.password=password
+    self.confirmPassword=confirmPassword
 class Credentials:
+    '''
+    '''
