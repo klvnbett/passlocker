80ba98868aa66f1a12154a7f49dc69b9a18e66e8
BettK <kelvin.bett@student.moringaschool.com>
2021-08-01 16:01:26 +0300

    added the create user def

--- pass.py
+++ pass.py
@@ -2,10 +2,21 @@ class User:
     '''
     this is a class for adding user 
     '''
-    def__init__(self,email,password,confirmPassword)
-    self.email=email
-    self.password=password
-    self.confirmPassword=confirmPassword
+
+    users=[] #empty users
+
+    def __init__(self,userName,email,password,confirmPassword):
+        self.email=email
+        self.password=password
+        self.confirmPassword=confirmPassword
+
+    def createUser(email,password,confirmPassword):
+        '''
+        an instance that will create a new user
+        '''
+        newUser =User(email,userName,password,confirmPassword)
+        return newUser
+
 class Credentials:
     '''
     '''
