2ae6d8947b0ab16a3cddb510801e76b66c47e796
BettK <kelvin.bett@student.moringaschool.com>
2021-08-01 19:28:46 +0300

    added the display and test several users

--- pass_test.py
+++ pass_test.py
@@ -18,8 +18,24 @@ def test_init(self):
     self.assertEqual(self.user.confirmPassword,"7890")
 
 def tearDown(self):
-    '''.git/method to clear list after every test
     '''
+    method to clear list after every test
+    '''
+    User.users =[]
+def test_severalUsers(self):
+    """
+    this will test the saved users
+    """
+    self.user=User("kbett","klvnbett@gmail.com","7890","7890")
+    self.user.saveUser(self.user.password)
+    self.assertEqual(len(User.users),1)
+
+def test_display_users(self):
+    self.user=User("kbett","klvnbett@gmail.com","7890","7890")
+    self.user.saveUser(self.user.password)
+    self.assertEqual(len(User.users),1)
+
+def test_delete_users(self):
 
 if __name__=='__main__':
  unittest.main()
