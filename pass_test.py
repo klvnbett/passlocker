ac4652bec78f2fe837770f66d44508a5b000e9a4
BettK <kelvin.bett@student.moringaschool.com>
2021-08-01 15:57:39 +0300

    added the self setup test

--- pass_test.py
+++ pass_test.py
@@ -3,3 +3,11 @@ class Test_userInput(unittest.TestCase):
     '''
     test class that defines test cases for user behaviours
     '''
+def setUp(self):
+    """
+    this method provide test input to test
+    """
+    self.user=User("klvnbett@gmail.com","7890","7890")
+
+if __name__=='__main__':
+ unittest.main()
