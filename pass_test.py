d8055621a0563966176c17460ae313e81feeefa6
BettK <kelvin.bett@student.moringaschool.com>
2021-08-01 16:31:32 +0300

    added the self test and delete test

--- pass_test.py
+++ pass_test.py
@@ -7,7 +7,19 @@ def setUp(self):
     """
     this method provide test input to test
     """
-    self.user=User("klvnbett@gmail.com","7890","7890")
+    self.user=User("kbett","klvnbett@gmail.com","7890","7890")
+def test_init(self):
+    """
+    method to perfom if correct initialization is met
+    """
+    self.assertEqual(self.user.userName,"kbett")
+    self.assertEqual(self.user.email,"klvnbett@gmail.com")
+    self.assertEqual(self.user.password,"7890")
+    self.assertEqual(self.user.confirmPassword,"7890")
+
+def tearDown(self):
+    '''.git/method to clear list after every test
+    '''
 
 if __name__=='__main__':
  unittest.main()
